2c2c6543c462b245f4ba6afc989f50af81349705
Abiaina <abinnet.a@gmail.com>
2018-04-02 22:57:06 -0700

    detects nil and non-palindrome

--- lib/integer_palindrome_check.rb
+++ lib/integer_palindrome_check.rb
@@ -1,4 +1,19 @@
 # Returns true if the input positive integer number forms a palindrome. Returns false otherwise.
 def is_palindrome(number)
-  raise NotImplementedError
+  if number == nil
+    return false
+  end
+
+  palindrome = false
+
+  # this should be multiples of 10.
+  x = 1
+
+  while number % x != 0
+    if number % x == number / x
+      palindrome == true
+    end
+    x *= 10
+  end
+  return palindrome
 end
