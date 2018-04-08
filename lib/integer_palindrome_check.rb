7362f5dfb68ba98ae980633d1e402d02c3dd46a5
Abiaina <abinnet.a@gmail.com>
2018-04-07 19:38:05 -0700

    detects odd and even digit count integer palindromes

--- lib/integer_palindrome_check.rb
+++ lib/integer_palindrome_check.rb
@@ -1,6 +1,5 @@
-require 'pry'
-
 def is_palindrome(number)
+
   if number == nil || number < 0
     return false
   elsif (0..9).cover?(number)
@@ -8,44 +7,45 @@ def is_palindrome(number)
   end
 
   range_by_ten = 10
-  even_odd_count = 0
 
   while (number / range_by_ten) > 0
     range_by_ten = (10 * range_by_ten)
-    even_odd_count += 1
   end
 
-  left_reflection = (range_by_ten / 10)
-  right_reflection = 10
+  get_left_side = (range_by_ten / 10)
+  get_right_side = 10
 
-  until left_reflection == right_reflection
-    if !((number / left_reflection) == (number % right_reflection))
+  get_first_digit = 1
+
+  until get_left_side < get_right_side
+
+    median_digits = get_left_side / get_right_side
+
+    left_side = (number / get_left_side) % 10
+    right_side = (number % get_right_side) / get_first_digit
+
+    if left_side != right_side
       return false
     else
-      left_reflection = (left_reflection / 10)
-      right_reflection = (right_reflection * 10)
-    end
-binding.pry
-    if ((left_reflection / right_reflection) == 100)
-      if odd_check(number) && ((number / left_reflection) == (number % right_reflection))
-        return true
-      end
+      get_left_side = (get_left_side / 10)
+      get_right_side = (get_right_side * 10)
+      get_first_digit = get_first_digit * 10
     end
   end
 
   return true
 end
 
-def odd_check(number)
-  number = number % 10
+# def odd_check(number)
+#   number = number % 10
+#
+#   is_odd = false
+#
+#   if number == 1 || number == 3 || number == 5 || number == 7 || number == 9
+#     is_odd = true
+#   end
+#
+#   return is_odd
+# end
 
-  is_odd = false
-
-  if number == 1 || number == 3 || number == 5 || number == 7 || number == 9
-    is_odd = true
-  end
-
-  return is_odd
-end
-
-is_palindrome(34143)
+puts is_palindrome(3411143)
